178ce7e7f0d2b2c3817f6a6aad3d9883f2e8ddd2
Ankit <ankitrawat0102@gmail.com>
2020-10-30 23:09:09 +0530

    Team section added

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-const navSlide = () => {
-    const burger = document.querySelector('.ham');
-    const nav = document.querySelector('.nav-links');
-
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('.nav-active');
-    })
-}
-
-navSlide();
+new Glider(document.querySelector('.glider'), {
+    slidesToShow: 2,
+    dots: '#dots',
+    arrows: {
+      prev: '.glider-prev',
+      next: '.glider-next'
+    }
+  });
